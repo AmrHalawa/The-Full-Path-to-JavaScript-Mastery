bf1586151abb21987cbf5b57de29b4bc1447c6f5
Amr Halawa <amrhalawa292@gmail.com>
2024-08-03 16:18:39 +0800

    String Length & Basic Properties

---
+++ 08_strings/script.js
@@ -0,0 +1,26 @@
+// const sum = (a, b) => a + b;
+
+// const total = `The sum is ${sum(2, 5)}`;
+
+// console.log(total);
+// console.log(typeof total);
+
+//----------------------------------------------//
+
+// const greeting = `Hi, I'm John, but people call me "Johnny".`;
+// console.log(greeting);
+
+//---------------------------------------------//
+
+// String Length & Basic Properties
+
+const firstName = "Amr is good.";
+console.log(firstName.length); // 3
+
+console.log(firstName[0]); // A  // access certain position of the character
+
+const firstLetter = firstName[0];
+// const lastLetter = firstName[2];
+const lastLetter = firstName[firstName.length - 1]; // .
+
+console.log(firstLetter, lastLetter);
