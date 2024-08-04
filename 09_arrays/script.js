ef863430c74c2f72ab50e91621aed7e4816ac66a
Amr Halawa <amrhalawa292@gmail.com>
2024-08-04 17:35:06 +0800

    Array map

--- 09_arrays/script.js
+++ 09_arrays/script.js
@@ -70,3 +70,23 @@ numbers.forEach((number) => {
 });
 
 console.log(sum);
+
+// -------------------------------------- //
+
+// Array Map --> allocates memory in order to store and return values
+
+// The for each method also allows for a callback function that will allow you to change the original array,
+// while the map method will instead return a new array while leaving the original one in its original state.
+
+const inventory = [
+  { price: 5, name: "eggs" },
+  { price: 4, name: "juice" },
+  { price: 3, name: "cheese" },
+  { price: 5, name: "bread" },
+];
+
+const prices = inventory.map((item) => item.price);
+const items = inventory.map((item) => item.name);
+
+console.log(prices); //(4) [5, 4, 3, 5]
+console.log(items); // (4) ['eggs', 'juice', 'cheese', 'bread']
