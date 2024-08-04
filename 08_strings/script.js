24bbd6256be52b00c643a6efe5b4134788da2247
Amr Halawa <amrhalawa292@gmail.com>
2024-08-04 12:04:19 +0800

    String Exercise

--- 08_strings/script.js
+++ 08_strings/script.js
@@ -100,3 +100,39 @@ console.log(dogSays.repeat(5));
 const email = " contact@javascript.com  ";
 
 console.log(email.trim()); // contact@javascript.com
+
+//---------------------------------------------//
+
+// Guests Exercise
+
+const guestList = "Our guests are: emma, jacob, isabella, ethan";
+
+// 1. Get a length of the string. Store it in a variable called length.
+
+const length = guestList.length;
+console.log(length); // 44
+
+// 2. Uppercase the entire string. Store the result in a variable called uppercasedGuestList.
+
+const uppercasedGuestList = guestList.toUpperCase();
+
+console.log(uppercasedGuestList); // OUR GUESTS ARE: EMMA, JACOB, ISABELLA, ETHAN
+
+// 3. Check whether 'ETHAN' is on the uppercasedGuestList. Store the answer in a variable called isEthanOnTheList.
+// The data type of the variable must be a boolean.
+
+const isEthanOnTheList = uppercasedGuestList.includes("ETHAN");
+
+console.log(isEthanOnTheList); // true
+
+// 4. Create a substring that only contains the following: 'EMMA, JACOB, ISABELLA, ETHAN'. Store the answer in a variable called substringGuests.
+
+const substringGuests = uppercasedGuestList.slice(16);
+
+console.log(substringGuests); // 'EMMA, JACOB, ISABELLA, ETHAN'
+
+// 5. Out of the substring you just created, create an array of names of people that are on the list. Store that array in a variable called guests.
+
+const guests = substringGuests.split(" ");
+
+console.log(guests); // [ 'EMMA', 'JACOB', 'ISABELLA', 'ETHAN']
